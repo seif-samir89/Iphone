a5dca97a16dcd46b9161c7f4c7050db8db843378
seif samir <117075048+seif-samir89@users.noreply.github.com>
2023-10-16 14:02:37 +0300

    Iphone

---
+++ main.js
@@ -0,0 +1,9 @@
+let imgs = document.querySelector('.imgs');
+let container = document.querySelector('.container');
+
+function phones(phone){
+    imgs.src = phone;
+}
+function colors(color){
+    container.style.background = color;
+}
